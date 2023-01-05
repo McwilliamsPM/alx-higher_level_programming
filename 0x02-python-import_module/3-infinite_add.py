b7c25fca09be63a774e90058b6959d137db1b696
Mcwilliams Prescouz <prescouzmcwilliams@gmail.com>
2023-01-05 06:12:04 -0800

    removed

--- 0x02-python-import_module/3-infinite_add.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    import sys
-    all = 0
-    for number in range(1, len(sys.argv)):
-        all += int(sys.argv[number])
-    print("{}".format(all))
